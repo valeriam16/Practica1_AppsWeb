13567751b22d16052b194cb7c32fa808649a119b
Valeria Morales <valeriamora728@gmail.com>
2024-05-14 16:57:57 -0600

    V1_Migracion

---
+++ Practica1/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('lastname_p');
+            $table->string('lastname_m');
+            $table->integer('age');
+            $table->date('birthdate');
+            $table->string('email')->unique();
+            $table->string('phone')->unique();
+            #$table->timestamp('email_verified_at')->nullable(); //por ver
+            $table->string('password');
+            $table->boolean('active')->default(false);
+            $table->rememberToken(); //por ver
+            $table->timestamps();
+            $table->string('latitude')->nullable();
+            $table->string('longitude')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
